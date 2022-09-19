ca09ca70e328144c1414ba252edcbccec30e85fb
Эдуард Крамаренко <eduardkramarenko@Eduards-MacBook-Pro.local>
2022-09-19 12:26:14 +0300

    fix all

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import React, { useState } from "react";
 import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
 import Main from "./components/main/Main";
-import RightBar from "./components/rigthbar/RightBar"
+import RightBar from "./components/rigthbar/RightBar";
 
+import "./styles/styles.css";
 export default function App() {
+  const [log, setLog] = useState(false);
+  const acc = () => setLog(!log);
 
-	const [log, setLog] = useState(false)
-
-	const acc = () => setLog(!log)
-
-	return (
-		<div >
-			<Header log={log} />
-			<div style={{ display: 'flex' }}>
-				<Navbar log={log} acc={acc} />
-				<Main />
-				<RightBar />
-			</div>
-		</div >
-	);
+  return (
+    <>
+      <Header log={log} />
+      <div style={{ display: "flex" }}>
+        <Navbar log={log} acc={acc} />
+        <Main />
+        <RightBar />
+      </div>
+    </>
+  );
 }
--- src/components/rigthbar/RightBar.jsx
+++ src/components/rigthbar/RightBar.jsx
@@ -1,61 +1,30 @@
-import React, { useState } from 'react'
-import cl from './rightBar_component.css'
-import data from '../../data/rightbar.json'
+import React, { useState } from "react";
+import data from "../../data/rightbar.json";
+import styles from "./rightbar.module.css";
+import RightBarItem from "./RightBarItem";
 
 export default function RightBar() {
+  const [card, setCard] = useState(0);
 
-	const [card, setCard] = useState(0)
-	const [place, setPlace] = useState(false)
+  const openCard = (e) => {
+    if (e.currentTarget.id) return;
+    setCard(Number(e.currentTarget.getAttribute("data-index")));
+  };
 
-	const openCard = (e) => {
-		setCard(e.target.getAttribute('index'))
-		console.log(e.target.getAttribute('index'));
-	}
-
-	const saveLoc = () => {
-		setPlace(!place)
-	}
-
-
-	return (
-		<div className='rightBar'>
-			{data.map(({ img, name, loc, coast }, index) => {
-				return (
-					<div className={`card ${card == index ? 'active' : ''}`}
-						key={index}
-						index={index}
-						onClick={openCard}>
-
-						<img className={`img ${card == index ? 'active' : ''}`}
-							src={img} alt="" />
-
-						<div className={`info ${card == index ? 'active' : ''}`}>
-							<div>
-								<div className={`name ${card == index ? 'active' : ''}`}>
-									{name}
-								</div>
-								<div className={`loc ${card == index ? 'active' : ''}`}>
-									<img src="/assets/rightbar/location.svg" alt="" />
-									{loc}
-								</div>
-							</div>
-
-							<div className='box' onClick={saveLoc}>
-								<div className={`box-logo ${place ? 'active' : ''}`}></div>
-							</div>
-						</div>
-
-						<div className={`booking ${card == index ? 'active' : ''}`}>
-							<div>
-								<span>per/person</span>
-								<div className={`coast ${card == index ? 'active' : ''}`}>{coast}</div>
-							</div>
-							<button className={`button ${card == index ? 'active' : ''}`}>Book Travel</button>
-						</div>
-
-					</div>
-				)
-			})}
-		</div >
-	)
+  return (
+    <div className={styles.rightBar}>
+      {data.map(({ img, name, loc, coast }, index) => (
+        <RightBarItem
+          cardNumber={card}
+          img={img}
+          name={name}
+          loc={loc}
+          coast={coast}
+          index={index}
+          key={name + index}
+          openCard={openCard}
+        />
+      ))}
+    </div>
+  );
 }
